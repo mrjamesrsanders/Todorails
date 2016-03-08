cbbdb4d9a85cff21b229f1f8607c79775df79fbc
Jason Horine <outbackdog@gmail.com>
2016-03-08 15:33:03 -0600

    create is now working... thanks, Mike

--- todo_rails/app/controllers/lists_controller.rb
+++ todo_rails/app/controllers/lists_controller.rb
@@ -12,10 +12,23 @@ class ListsController < ApplicationController
   end
 
   def create
-    list = List.new(params["list"])
+    list = List.new(list_params)
     list.save
-    redirect '/show/(:id => #{this.id})'
+    # redirect '/show/(:id => #{this.id})'
+    redirect_to list
   end
+
+private
+    # Use callbacks to share common setup or constraints between actions.
+    # def set_growl
+    #   @growl = Growl.find(params[:id])
+    # end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def list_params
+      params.require(:list).permit(:name)
+    end
+
 end
 
 # But if you had something like hello_path then you
