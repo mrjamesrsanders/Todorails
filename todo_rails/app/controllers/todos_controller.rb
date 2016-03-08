b7f8d949087fec041044e73199ad70f32dc0d0b7
Jason Horine <outbackdog@gmail.com>
2016-03-08 16:40:40 -0600

    it's working

--- todo_rails/app/controllers/todos_controller.rb
+++ todo_rails/app/controllers/todos_controller.rb
@@ -3,12 +3,29 @@ class TodosController < ApplicationController
   end
 
   def new
+    @todo = Todo.new
   end
 
   def show
   end
 
   def create
+    todo = Todo.new(todo_params)
+    todo.save
+    # binding.pry
+    redirect_to todo.list
   end
+
+private
+    # Use callbacks to share common setup or constraints between actions.
+    # def set_growl
+    #   @growl = Growl.find(params[:id])
+    # end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def todo_params
+      params.require(:todo).permit(:item, :list_id)
+    end
+
 end
-\'
+
