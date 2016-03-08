766b8787a39bba4c618f833eca074f5868ce89df
James Sanders <mrjamesrsanders@gmail.com>
2016-03-08 13:04:20 -0600

    worked on lists_controller.rb and index.html.erb

--- todo_rails/app/controllers/lists_controller.rb
+++ todo_rails/app/controllers/lists_controller.rb
@@ -1,11 +1,13 @@
 class ListsController < ApplicationController
   def index
+      @lists = List.all
   end
 
   def new
   end
 
   def show
+      @list = List.find(params[:id])
   end
 
   def create
